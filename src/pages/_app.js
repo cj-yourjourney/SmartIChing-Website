12f249941dea74d4de6a032c370075c0117996ba
cj <49462194+cj-yourjourney@users.noreply.github.com>
2026-07-22 15:59:49 -0700

    added google anltic in the head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,11 @@
 import { Provider } from 'react-redux'
 import { store } from '@/shared/redux/store'
 import Navbar from '@/shared/components/Navbar'
-import { GoogleTagManager } from '@next/third-parties/google'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <GoogleTagManager gtmId="G-2FVTDEEQEJ" />
       <Navbar />
       <Component {...pageProps} />
     </Provider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2FVTDEEQEJ"
+        />
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2FVTDEEQEJ');
+            `
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
