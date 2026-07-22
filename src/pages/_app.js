1507c149cf2809ea1fdeebc575066e5368dc0dd7
cj <49462194+cj-yourjourney@users.noreply.github.com>
2026-07-22 16:51:32 -0700

    removeed the tag in app file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import posthog from 'posthog-js'
 import { Provider } from 'react-redux'
 import { store } from '@/shared/redux/store'
 import Navbar from '@/shared/components/Navbar'
-import { GoogleTagManager } from '@next/third-parties/google'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -21,7 +20,6 @@ export default function App({ Component, pageProps }) {
 
   return (
     <Provider store={store}>
-      <GoogleTagManager gtmId="G-2FVTDEEQEJ" />
       <Navbar />
       <Component {...pageProps} />
     </Provider>
