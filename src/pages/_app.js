9006d639fd7ca0ee39ca76319aa99b2a51b463b8
cj <49462194+cj-yourjourney@users.noreply.github.com>
2026-07-22 16:14:45 -0700

    added hogpost

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,27 @@
+import { useEffect } from 'react'
+import posthog from 'posthog-js'
 import { Provider } from 'react-redux'
 import { store } from '@/shared/redux/store'
 import Navbar from '@/shared/components/Navbar'
+import { GoogleTagManager } from '@next/third-parties/google'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !posthog.__loaded) {
+      posthog.init('phc_RVwdiW3q1GG2KWzeAnxGuMFNPBbq53am6SgCZ5oYnJJ', {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only',
+        loaded: (ph) => {
+          ph.register({ app: 'smartiching' })
+        }
+      })
+    }
+  }, [])
+
   return (
     <Provider store={store}>
+      <GoogleTagManager gtmId="G-2FVTDEEQEJ" />
       <Navbar />
       <Component {...pageProps} />
     </Provider>
